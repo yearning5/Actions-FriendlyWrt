6f23612cb80f3cd6938ca65c3981bb1a901e485b
yearning5 <yearning_5@yahoo.fr>
2026-02-21 22:58:57 +0100

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -34,14 +34,17 @@ sed -i -e '/boardname=/r /tmp/appendtext.txt' friendlywrt/target/linux/rockchip/
 # Add OpenAppFilter
 (cd friendlywrt && {
     git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter --depth 1
-    echo "CONFIG_PACKAGE_open-app-filter=y" >> configs/rockchip/01-nanopi
-    echo "CONFIG_PACKAGE_luci-app-oaf=y" >> configs/rockchip/01-nanopi
-    echo "CONFIG_PACKAGE_kmod-oaf=y" >> configs/rockchip/01-nanopi
 })
+
+# Add OAF config after build setup
+cat >> scripts/customize_config.sh << 'EOFCUSTOM'
+echo "CONFIG_PACKAGE_open-app-filter=y" >> configs/rockchip/01-nanopi
+echo "CONFIG_PACKAGE_luci-app-oaf=y" >> configs/rockchip/01-nanopi
+echo "CONFIG_PACKAGE_kmod-oaf=y" >> configs/rockchip/01-nanopi
+echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> configs/rockchip/01-nanopi
+EOFCUSTOM
 
 # Add AdGuard Home
 (cd friendlywrt && {
     git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome --depth 1
-    echo "CONFIG_PACKAGE_adguardhome=y" >> configs/rockchip/01-nanopi
-    echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> configs/rockchip/01-nanopi
-}) 
+})
